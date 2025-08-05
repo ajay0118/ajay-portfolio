5f1504e14ca1925b0ec54081dc9a4ecfbb0e6074
AJAYVENKATESH PS <ajayvenkateshps@AJAYVENKATESHs-MacBook-Air.local>
2025-08-05 09:43:05 +0530

    Updated Navbar section content-2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,16 +11,18 @@ function About() {
 
                 <h3>Skills</h3>
                 <ul className='skills-list'>
+                    <li><img src="/icons/java.png" alt="Java" /> Java</li>
                     <li><img src="/icons/html.png" alt="HTML5" /> HTML5</li>
                     <li><img src="/icons/css.png" alt="CSS" /> CSS</li>
                     <li><img src="/icons/javascript.png" alt="JavaScript" /> JavaScript</li>
                     <li><img src="/icons/reactjs.png" alt="React" /> React.js</li>
-                    <li><img src="/icons/java.png" alt="Java" /> Java</li>
+                    <li><img src="/icons/nodejs.png" alt="Node.js" /> Node.js</li>
                     <li><img src="/icons/mysql.png" alt="MySQL" /> MySQL</li>
                     <li><img src="/icons/mongodb.png" alt="MongoDB" /> MongoDB</li>
                     <li><img src="/icons/github.png" alt="GitHub" /> GitHub</li>
-                    <li><img src="/icons/cnn.png" alt="CNN" /> CNN</li>
                     <li><img src="/icons/ml.png" alt="Machine Learning" /> Machine Learning</li>
+                    <li><img src="/icons/cnn.png" alt="CNN" /> CNN</li>
+                    <li><img src="/icons/db.png" alt="Database" /> Database</li>
                 </ul>  
             </div>
         </section>
